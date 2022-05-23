068c2f32f17ec88dc03ca8156bf48dee906e0192
Alexander Bondarev <alex@strangeloopgames.com>
2022-05-23 14:48:27 +0400

    Add Telegram login

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,17 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+
+define('telegram_config', ["token" => env("TELEGRAM_BOT_TOKEN")]);
+
 
 class UserController extends Controller
 {
+    /**
+     * @throws Exception
+     */
+    function checkAuth(array $auth_data): array
+    {
+        $bot_token = telegram_config["token"];
+        $check_hash = $auth_data["hash"];
+        unset($auth_data["hash"]);
+        $data_check_arr = [];
+        foreach ($auth_data as $key => $value) {
+            $data_check_arr[] = $key . '=' . $value;
+        }
+        sort($data_check_arr);
+        $data_check_string = implode("\n", $data_check_arr);
+        $secret_key = hash('sha256', $bot_token, true);
+        $hash = hash_hmac('sha256', $data_check_string, $secret_key);
+        if (strcmp($hash, $check_hash) !== 0) {
+            throw new Exception('Data is NOT from Telegram');
+        }
+        if ((time() - $auth_data['auth_date']) > 86400) {
+            throw new Exception('Data is outdated');
+        }
+        return $auth_data;
+    }
+
     public function login(Request $request)
     {
-        $user = User::where('name', $request->post('username'))->first();
+        $auth_data = $this->checkAuth($request->json()->all());
+        $username = $auth_data['username'];
+        $user = User::where('name', $username)->first();
 
         if (!$user) {
             $user = new User();
-            $user->name = $request->post('username');
+            $user->name = $username;
             $user->email = Carbon::now()->getTimestamp() . '@dispostable.com';
             $user->email_verified_at = Carbon::now();
             $user->password = md5(bin2hex(random_bytes(40)));
--- src/client/src/components/LoginPopup.js
+++ src/client/src/components/LoginPopup.js
@@ -3,18 +3,17 @@ import React, { useState } from 'react';
 import { useRecoilState, } from 'recoil';
 import { Button, Form, InputGroup, Modal } from 'react-bootstrap';
 import CurrentUserAtom from "../recoil/atoms/CurrentUserAtom";
+import TelegramLoginButton from 'react-telegram-login';
 
 function LoginPopup() {
     const [user, setUser] = useRecoilState(CurrentUserAtom);
     const [username, setUsername] = useState(window.localStorage.getItem('username'));
     const [role, setRole] = useState('Admin');
 
-    const doLogin = async () => {
-        const {data} = await axios.post('/user/login', {
-            username
-        });
+    const doLogin = async (telegramResponse) => {
+        const {data} = await axios.post('/user/login', telegramResponse);
 
-        window.localStorage.setItem('username', username);
+        window.localStorage.setItem('username', telegramResponse.username);
 
         axios.defaults.headers.common['Authorization'] = data.token;
 
@@ -25,34 +24,17 @@ function LoginPopup() {
         });
     };
 
+    const handleTelegramResponse = response => {
+        console.log(response);
+    }
+
     return (<Modal
         show={true}
         backdrop="static"
         keyboard={false}
     >
         <Modal.Body>
-            <Form>
-                <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Telegram name</Form.Label>
-                    <InputGroup>
-                        <InputGroup.Text>@</InputGroup.Text>
-                        <Form.Control type="text" placeholder="name"
-                                      value={username}
-                                      onChange={(Affair) => setUsername(Affair.target.value)} />
-                    </InputGroup>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Label>Role</Form.Label>
-                    <Form.Check type="radio" name="role" label="Admin" checked={role === 'Admin'} onChange={() => setRole('Admin')} />
-                    <Form.Check type="radio" name="role" label="Authorised" checked={role === 'Authorised'} onChange={() => setRole('Authorised')} />
-                    <Form.Check type="radio" name="role" label="Guest" checked={role === 'Guest'} onChange={() => setRole('Guest')} />
-                </Form.Group>
-
-                <Button variant="primary" type="button" onClick={doLogin}>
-                    Login
-                </Button>
-            </Form>
+            <TelegramLoginButton dataOnauth={doLogin} botName="pratchet_bot" />
         </Modal.Body>
     </Modal>);
 }
